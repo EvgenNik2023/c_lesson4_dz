34d297674c0b780cc238400346be6d1de706d6c7
evgen <makeevgen1975@gmail.com>
2023-09-05 10:15:56 +0300

    add task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+int Input()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int number = Input();
+
+int Sum(int number)
+{
+    int sum = 0;
+    if (number < 0) number *= -1;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+int sum = Sum(number);
+
+Console.WriteLine("Сумма цифр числа = " + sum);
